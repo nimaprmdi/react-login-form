681c122d95609a9c98fdd44772f48e2cc0c502c6
nimaprmdi <nimaprmdi@gmail.com>
2022-07-21 20:02:47 +0430

    handle Functional Component based login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,83 +1,55 @@
 import React, { useState } from "react";
 import Joi from "joi-browser";
 import Input from "./common/Input";
+import {
+    data,
+    setData,
+    allErrors,
+    setAllErrors,
+    validate,
+    validateProperty,
+    handleChange,
+    handleSubmit,
+} from "./common/Form";
 
 const LoginForm = () => {
-    const [account, setAccount] = useState({
+    const [data, setData] = useState({
         username: "",
         password: "",
     });
 
-    const [allErrors, setAllErrors] = useState({}); // Global errors state - The Primary One
+    const [allErrors, setAllErrors] = useState({});
 
     const schema = {
         username: Joi.string().required().label("Username"),
         password: Joi.string().required().min(4).label("Password"),
     };
 
-    const validate = () => {
-        const options = { abortEarly: false };
-
-        const { error } = Joi.validate(account, schema, options);
-
-        if (!error) return null;
-
-        const errors = {};
-        for (let item of error.details) {
-            errors[item.path[0]] = item.message;
-        }
-
-        return errors;
-    };
-
-    const validateProperty = ({ name, value }) => {
-        const obj = { [name]: value };
-        const localSchema = { [name]: schema[name] };
-        const { error } = Joi.validate(obj, localSchema);
-        return error ? error.details[0].message : null;
-    };
-
-    const handleChange = ({ currentTarget: input }) => {
-        // Error checker
-        const errors = { ...allErrors }; // Copy all errors {state}
-        const errorsMessage = validateProperty(input); // validate input using given function
-        if (errorsMessage) errors[input.name] = errorsMessage; // set errors => "errors.username" to "errorMessage"
-        else delete errors[input.name];
-
-        // Changing Inputs
-        const accountClone = { ...account }; // Clone account state
-        accountClone[input.name] = input.value; // Change account {state} value with real input value
-        setAccount(accountClone); // Set global account state
-        setAllErrors(errors);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault(); // Prevent Refreshing page
-
-        const errors = validate(); // Handle State errors and put it in the errors variable
-        setAllErrors(errors || {}); // Set Errors {State} to errors variable - If was empty set to empty object
-        if (errors) return; // If errors was true then do nothing
+    const doSubmit = () => {
+        console.log("Submit");
     };
 
     return (
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={(e) => handleSubmit(e, data, setAllErrors, doSubmit, schema)}>
             <Input
                 name="username"
-                value={account.username}
+                value={data.username}
                 error={allErrors.username}
-                onChange={(e) => handleChange(e)}
+                onChange={(e) => handleChange(e, data, setData, allErrors, setAllErrors, schema)}
                 label="Username"
             />
 
             <Input
                 name="password"
-                value={account.password}
+                value={data.password}
                 error={allErrors.password}
-                onChange={(e) => handleChange(e)}
+                onChange={(e) => handleChange(e, data, setData, allErrors, setAllErrors, schema)}
                 label="Password"
             />
 
-            <button className="btn btn-primary bt-lg">Submit</button>
+            <button disabled={validate(data, schema)} className="btn btn-primary bt-lg">
+                Submit
+            </button>
         </form>
     );
 };
---
+++ src/components/common/Form.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Joi from "joi-browser";
+
+const validate = (data, schema) => {
+    const options = { abortEarly: false }; // aboirtEarly false will display all errors and not stop because of one error
+    const { error } = Joi.validate(data, schema, options); // Validate doSubmit (function) using data and schema
+    if (!error) return null; // Leave alone if we dont have any error
+    const errors = {}; // Local error object
+    for (let item of error.details) {
+        errors[item.path[0]] = item.message; // get indexed path for example [username] and set it to message => error{ username: "Error Message" }
+    }
+
+    return errors; // It's Obvious
+};
+
+const validateProperty = ({ name, value }, schema) => {
+    const obj = { [name]: value }; // Create an object using name input (Dynamically) and value static ( [username] : nima )
+    const localSchema = { [name]: schema[name] }; // create a schema using dynamic input name and get the index of schemal(outside) state => [username] : schema.username
+    const { error } = Joi.validate(obj, localSchema); // Validate data using given data (obj) and localSchema[Indexed Schema]
+    return error ? error.details[0].message : null; // If we have error return otherwise nothing
+};
+
+const handleChange = ({ currentTarget: input }, data, setData, allErrors, setAllErrors, schema) => {
+    // Error checker
+    const errors = { ...allErrors }; // Copy all errors {state}
+    const errorsMessage = validateProperty(input, schema); // validate input using given function
+    if (errorsMessage) errors[input.name] = errorsMessage; // set errors => "errors.username" to "errorMessage"
+    else delete errors[input.name]; // remove key of object if we dont have an error
+
+    // Changing Inputs
+    const dataClone = { ...data }; // Clone data state
+    dataClone[input.name] = input.value; // Change data {state} value with real input value
+    setData(dataClone); // Set global data state
+    setAllErrors(errors); // Set global errors state
+};
+
+const handleSubmit = (e, data, setAllErrors, doSubmit, schema) => {
+    e.preventDefault(); // Prevent Refreshing page
+
+    const errors = validate(data, schema); // Handle State errors and put it in the errors variable
+    setAllErrors(errors || {}); // Set Errors {State} to errors variable - If was empty set to empty object
+    if (errors) return; // If errors was true then do nothing
+
+    doSubmit(); // Outside do submit ( The api for example ) => Mixed Function
+};
+
+export { validate, validateProperty, handleChange, handleSubmit };
